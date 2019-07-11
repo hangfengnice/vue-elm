84e1882c0c46974b6907fb9629bf3cf47cc6dc16
hangfengnice <nicehangfeng@foxmail.com>
2019-07-11 19:10:17 +0800

    elm-start

--- client/README.md
+++ client/README.md
@@ -1,4 +1,4 @@
-# vue-todo-manager
+# client
 
 ## Project setup
 ```
--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import todos from './modules/todos'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    todos
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
   }
 })
