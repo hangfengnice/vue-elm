c974c545bdbe2af2b136b7bb53d14a8dd7097620
hangfengnice <nicehangfeng@foxmail.com>
2019-07-14 10:05:44 +0800

    client-swipe
    引入mint-ui 使用swipe

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import router from './router'
 import store from './store'
 import './assets/reset.css'
 import axios from 'axios'
+
+import { Indicator } from 'mint-ui';
+
+Vue.use(MintUI)
+
+axios.interceptors.request.use(config => {
+  // 加载动画
+  Indicator.open()
+  return config
+},error => {
+  return Promise.reject(error)
+})
+
+// 响应拦截
+axios.interceptors.response.use(response => {
+  // 加载动画
+  Indicator.close()
+  return response
+},error => {
+  Indicator.close()
+  return Promise.reject(error)
+})
+
 
 Vue.config.productionTip = false
 
